f5982d2bb17278035c60fdf0356bdb5b12c63153
Urban-JonathanCohen <51330244+Urban-JonathanCohen@users.noreply.github.com>
2021-05-04 15:52:55 +0200

    staff_n_waste_gen

--- WASTE_DB/w2r.sql
+++ WASTE_DB/w2r.sql
@@ -1,6 +1,6 @@
 -- SQL dump generated using DBML (dbml-lang.org)
 -- Database: PostgreSQL
--- Generated at: 2021-04-28T20:03:14.244Z
+-- Generated at: 2021-05-04T12:12:42.421Z
 
 CREATE TYPE "geom_enum" AS ENUM (
   'Point',
@@ -23,36 +23,35 @@ CREATE TYPE "frequency" AS ENUM (
   'Weekly'
 );
 
+CREATE TYPE "week_day" AS ENUM (
+  'Monday',
+  'Tuesday',
+  '...'
+);
+
 CREATE TABLE "agent" (
   "agent_id" SERIAL UNIQUE PRIMARY KEY,
-  "q_members" int,
+  "name" varchar,
+  "size" int,
   "agent_activity" varchar,
   "nace_code" varchar,
+  "type_of_agent" varchar,
+  "oublic" varchar,
+  "membership_name" varchar,
+  "membership_id" int,
   "income" int,
   "latitude" int,
   "longitude" int,
+  "address" varchar,
   "geom" geom_enum,
   "date_updated" timestamp,
   "data_source" varchar
 );
 
-CREATE TABLE "member_connect" (
-  "agent_id" int,
-  "membership" int
-);
-
-CREATE TABLE "membership" (
-  "membership_id" int PRIMARY KEY,
-  "q_members" int,
-  "type_of_org" varchar,
-  "budget" int,
-  "address" int,
-  "description" varchar
-);
-
-CREATE TABLE "generation" (
+CREATE TABLE "generator" (
   "gen_id" int PRIMARY KEY,
   "agent_id" int,
+  "waste_id" int,
   "resource_used" varchar,
   "q_resource" int,
   "resource_freq" frequency,
@@ -82,7 +81,7 @@ CREATE TABLE "consumer" (
   "data_source" varchar
 );
 
-CREATE TABLE "storage" (
+CREATE TABLE "storer" (
   "store_id" int PRIMARY KEY,
   "agent_id" int,
   "total_capacity" int,
@@ -93,7 +92,7 @@ CREATE TABLE "storage" (
   "data_source" varchar
 );
 
-CREATE TABLE "tranformation" (
+CREATE TABLE "recycler" (
   "trans_id" int PRIMARY KEY,
   "agent_id" int,
   "waste_type" int,
@@ -110,18 +109,25 @@ CREATE TABLE "tranformation" (
   "data_source" varchar
 );
 
-CREATE TABLE "transportation" (
+CREATE TABLE "transporter" (
   "transport_id" int PRIMARY KEY,
   "agent_id" int,
   "waste_type" int,
-  "waste_qty" int,
-  "waste_frq" frequency,
+  "waste_capacity" int,
   "travel_mode" int,
   "capacity" int,
-  "vehicle" varchar,
+  "vehicle_brand" varchar,
   "vehicle_model" varchar,
   "vehicle_url" varchar,
   "age" int,
+  "date_updated" timestamp,
+  "data_source" varchar
+);
+
+CREATE TABLE "waste" (
+  "waste_id" SERIAL UNIQUE PRIMARY KEY,
+  "type_of_waste" varchar,
+  "waste_qty" int,
   "date_updated" timestamp,
   "data_source" varchar
 );
@@ -132,30 +138,19 @@ CREATE TABLE "route" (
   "latitude" int,
   "longitude" int,
   "sequence" int,
+  "frequency" frequency,
+  "day_of_the_week" week_day,
   "moment" timestamp,
   "job_type" varchar,
   "date_updated" timestamp,
   "data_source" varchar
 );
 
-CREATE TABLE "resource" (
-  "res_id" SERIAL UNIQUE PRIMARY KEY,
-  "resource_log_id" int,
-  "resource_type" varchar,
-  "res_qty" int,
-  "resource_p" int,
-  "price_tag_source" varchar,
-  "date_updated" timestamp,
-  "data_source" varchar
-);
-
-CREATE TABLE "waste" (
-  "waste_id" SERIAL UNIQUE PRIMARY KEY,
-  "waste_log_id" int,
-  "type_of_waste" varchar,
-  "waste_qty" int,
-  "date_updated" timestamp,
-  "data_source" varchar
+CREATE TABLE "gps" (
+  "gps_id" int PRIMARY KEY,
+  "vehicle_id" int,
+  "latitude" int,
+  "longitude" int
 );
 
 CREATE TABLE "process" (
@@ -175,11 +170,11 @@ CREATE TABLE "process" (
   "data_source" varchar
 );
 
-CREATE TABLE "waste_log" (
-  "waste_log_id" int PRIMARY KEY,
+CREATE TABLE "waste_unit" (
+  "waste_id" int PRIMARY KEY,
   "agent_id" int,
   "contain_id" int,
-  "created" timestamp,
+  "moment_of_operaton" timestamp,
   "waste_type" varchar,
   "low_code" varchar,
   "quantity" int,
@@ -226,33 +221,42 @@ CREATE TABLE "kpi" (
   "zones_id" int
 );
 
-ALTER TABLE "member_connect" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+CREATE TABLE "resource" (
+  "res_id" SERIAL UNIQUE PRIMARY KEY,
+  "resource_log_id" int,
+  "resource_type" varchar,
+  "res_qty" int,
+  "resource_p" int,
+  "price_tag_source" varchar,
+  "date_updated" timestamp,
+  "data_source" varchar
+);
 
-ALTER TABLE "member_connect" ADD FOREIGN KEY ("membership") REFERENCES "membership" ("membership_id");
+ALTER TABLE "agent" ADD FOREIGN KEY ("membership_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "generation" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+ALTER TABLE "generator" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+
+ALTER TABLE "waste" ADD FOREIGN KEY ("waste_id") REFERENCES "generator" ("waste_id");
 
 ALTER TABLE "consumer" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "storage" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+ALTER TABLE "storer" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "tranformation" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+ALTER TABLE "recycler" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "transportation" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+ALTER TABLE "transporter" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "transportation" ADD FOREIGN KEY ("transport_id") REFERENCES "route" ("transport_id");
+ALTER TABLE "transporter" ADD FOREIGN KEY ("transport_id") REFERENCES "route" ("transport_id");
 
-ALTER TABLE "resource" ADD FOREIGN KEY ("resource_log_id") REFERENCES "resource_log" ("resource_log_id");
-
-ALTER TABLE "waste" ADD FOREIGN KEY ("waste_log_id") REFERENCES "waste_log" ("waste_log_id");
+ALTER TABLE "transporter" ADD FOREIGN KEY ("transport_id") REFERENCES "gps" ("vehicle_id");
 
 ALTER TABLE "process" ADD FOREIGN KEY ("waste_id") REFERENCES "waste" ("waste_id");
 
 ALTER TABLE "process" ADD FOREIGN KEY ("resource_id") REFERENCES "resource" ("res_id");
 
-ALTER TABLE "waste_log" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
+ALTER TABLE "waste_unit" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
-ALTER TABLE "container" ADD FOREIGN KEY ("cont_id") REFERENCES "waste_log" ("contain_id");
+ALTER TABLE "container" ADD FOREIGN KEY ("cont_id") REFERENCES "waste_unit" ("contain_id");
 
 ALTER TABLE "resource_log" ADD FOREIGN KEY ("agent_id") REFERENCES "agent" ("agent_id");
 
@@ -264,15 +268,13 @@ ALTER TABLE "container" ADD FOREIGN KEY ("cont_id") REFERENCES "zones" ("contain
 
 ALTER TABLE "zones" ADD FOREIGN KEY ("zones_id") REFERENCES "kpi" ("zones_id");
 
-COMMENT ON TABLE "agent" IS 'Spatial class that contains information of stakeholder related to waste and resources';
+ALTER TABLE "resource" ADD FOREIGN KEY ("resource_log_id") REFERENCES "resource_log" ("resource_log_id");
 
-COMMENT ON TABLE "member_connect" IS 'Many-2-many relationships are not allowed. This is a table to create these sort of relations';
+COMMENT ON TABLE "agent" IS 'Spatial class that contains shared information between the stakeholder related to waste and resources';
 
-COMMENT ON COLUMN "generation"."process_url" IS 'url of procees';
+COMMENT ON COLUMN "generator"."process_url" IS 'url of procees';
 
-COMMENT ON TABLE "resource" IS 'This table contains information about resource present in the system';
-
-COMMENT ON COLUMN "resource"."price_tag_source" IS 'url of price';
+COMMENT ON TABLE "transporter" IS 'Describing the asset that make the movemnet of waste happen';
 
 COMMENT ON TABLE "waste" IS 'This table contains information about the secondary resource present in the system';
 
@@ -283,3 +285,7 @@ COMMENT ON COLUMN "process"."total_cost" IS 'cost of the process. Res2Waste or W
 COMMENT ON TABLE "container" IS 'This table contains information about the containers in the system';
 
 COMMENT ON COLUMN "container"."container_image" IS 'This is a file for an image of the container';
+
+COMMENT ON TABLE "resource" IS 'This table contains information about resource present in the system';
+
+COMMENT ON COLUMN "resource"."price_tag_source" IS 'url of price';
